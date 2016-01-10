54283f2edb7df384c52dae8c927d05855ebcc33d
Michael <michael_kuan@hiiir.com>
2016-01-10 21:25:02 +0800

    Fix test case.

--- tests/motor.js
+++ tests/motor.js
@@ -1,14 +1,18 @@
-import mraa
-import time
+var mraa = require('mraa');
 
 x = mraa.Pwm(10)
 x.period_us(700)
-x.enable(True)
+x.enable(true)
 value= 0.0
 
-while True:
+function run() {
     x.write(value)
-    time.sleep(0.05)
     value = value + 0.01
-    if value >= 1:
+    if (value >= 1) {
         value = 0.0
+    }
+
+    setTimeout(run, 50);
+}
+
+run();
